7e2f0153908c12cff6867cec7b2fbb6cbae66649
lycliyichao <yichao@YICHAOs-Macbook-Pro-14.local>
2024-01-04 20:48:23 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,11 +14,17 @@ let package = Package(
             name: "AMLLView",
             targets: ["AMLLView"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AMLLView"),
+            name: "AMLLView",
+            dependencies: [
+                .product(name: "SwiftSoup", package: "SwiftSoup")
+            ]),
         .testTarget(
             name: "AMLLViewTests",
             dependencies: ["AMLLView"]),
